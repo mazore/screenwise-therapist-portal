23c1b52b76d9c73221c5a47314c57b7a2dffc420
Evan Mazor <evanmazor@gmail.com>
2025-05-15 15:30:27 -0700

    Comment out auth

--- src/pages/RequireAuth.tsx
+++ src/pages/RequireAuth.tsx
@@ -3,9 +3,9 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const RequireAuth = () => {
   const isAuthenticated = useIsAuthenticated();
-  if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
-  }
+//   if (!isAuthenticated) {
+//     return <Navigate to="/login" replace />;
+//   }
   return <Outlet />;
 };
 
